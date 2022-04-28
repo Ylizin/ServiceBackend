4c304aba1d0fc325ee4225fd760031aac7271895
ylizin <ylizin@163.com>
2022-04-28 20:50:50 +0800

    update cors

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from typing import Optional
 from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 import logging 
 logger = logging.getLogger("gunicorn.error")
 
 
 app = FastAPI()
 
-
+origins = [
+    "*"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 from model import ClassifyResponseModel,RequestInfo,ResponseStatus,MatchResponseModel
