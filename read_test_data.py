a688cf8a4488e192ea2d392897ef93f9a388170e
ylizin <ylizin@163.com>
2022-04-30 12:25:06 +0800

    update

--- read_test_data.py
+++ read_test_data.py
@@ -1,6 +1,5 @@
 #%%
 import pandas as pd
-from requests import head
 
 tag2servs = pd.read_csv('tag_servs.csv',header = None,index_col = 0)
 pw_df = pd.read_csv('filtered_datas.csv',index_col=0).loc[tag2servs.index]
